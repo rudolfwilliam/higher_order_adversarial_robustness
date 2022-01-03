4952847f3515e047261a2ff0ab2e6f360c8b31d5
bauerem <bauerem@student.ethz.ch>
2022-01-03 21:29:33 +0100

    Removed a bunch of bugs and added 8th order accuracy

--- CURE_robustness_mod/utils/config.py
+++ CURE_robustness_mod/utils/config.py
@@ -1,17 +1,10 @@
-# Hessian vector product accuracies
-# LOW corresponds to original CURE
-LOW = 0
-MED = 1
-HIGH = 2
-
-
 CIFAR_CONFIG = {
     # Constants
-    "device": "cpu",
+    "device": "cuda",
 
     # Data parameters
     "batch_size_train": 100,  # E: Bugs want this to be 100 for cifar
-    "batch_size_test": 1000,
+    "batch_size_test": 200,
     "shuffle_train": True,
     "image_min": 0,
     "image_max": 1,
@@ -21,20 +14,17 @@ CIFAR_CONFIG = {
     "model_name": 'ResNet18',  # SimpleModel or ResNet18
 
     # CURE configurations
-    "accuracy": MED,
+    "accuracy": 1,
     "lambda_0": 0,
     "lambda_1": 4,
-    "lambda_2": 0,
-    "mu_1": 0,
-    "mu_2": 0,
-    "mu_3": 0,
+    "lambda_2": 1,
     "h": [0.1, 0.5, 0.9, 1.3, 1.5],
     "optimization_algorithm": 'Adam',
     "optimizer_arguments": {
         'lr': 1e-4
     },
-    "epochs": 20,
-    "epsilon": 8 / 255,
+    "epochs": 10,
+    "epsilon": 2 / 255,
 
     "use_checkpoint": False,
     "checkpoint_file": 'checkpoint_01.data'
